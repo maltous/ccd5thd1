e63389084da09278424010d0d58c958095a5dbe1
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-04-14 17:07:30 +0300

    ccd5thd1

---
+++ story_content/user.js
@@ -0,0 +1,39 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6AbmoR8emAX":
+        Script1();
+        break;
+      case "5iqRHv8Vpx2":
+        Script2();
+        break;
+      case "6rSfAfzNgUk":
+        Script3();
+        break;
+      case "5jZu0FVfn8X":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script4()
+{
+  window.print();
+}
+
